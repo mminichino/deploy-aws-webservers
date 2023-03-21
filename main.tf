d3cb7bfbbf36e8e234cee6eb396e06ad2da715b1
Michael Minichino <mminichino@mac.com>
2023-03-21 11:31:44 -0500

    first commit

--- main.tf
+++ main.tf
@@ -2,11 +2,12 @@
 
 module "services_webserver" {
   source  = "app.terraform.io/mm-orcd/services/aws//modules/webserver"
-  version = "0.0.3"
+  version = "0.0.4"
   aws_region = var.aws_region
   aws_zone = var.aws_zone
   cidr_block = var.cidr_block
   environment_name = var.environment_name
   ssh_key = var.ssh_key
+  ssh_private_key = var.ssh_private_key
   subnet_block = var.subnet_block
 }
